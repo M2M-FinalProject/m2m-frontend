f6914c804e0e314af820ac66f23663a6b24187ad
mirzapm13 <mirzapm13@gmail.com>
2022-10-10 16:43:49 +0700

    merge and configure navigation

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { useFonts } from "expo-font";
 import Home from './screens/Home';
 import MyMatch from './screens/MyMatch';
 import Participation from './screens/Participation';
@@ -9,11 +8,8 @@ import MatchRequest from './screens/MatchRequest';
 import { Provider } from 'react-redux'
 import store from './store';
 
-
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer} from '@react-navigation/native'
 import LandingPage from './screens/LandingPage'
 import RegisterPage from './screens/RegisterPage'
 import LoginPage from './screens/LoginPage'
@@ -22,25 +18,25 @@ import LoginPage from './screens/LoginPage'
 const Tab = createBottomTabNavigator()
 const Stack = createNativeStackNavigator()
 
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
-    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
-  });
+function HomeNavigator() {
+  return (
+    <Tab.Navigator screenOptions={{ headerTitleAlign: 'center', headerStyle: { backgroundColor: '#FAF9FA' } }}>
+      <Tab.Screen name='Home' component={Home} />
+      <Tab.Screen name='Products' component={Products} />
+      <Tab.Screen name='Profile' component={Profile} />
+    </Tab.Navigator>
+  )
+}
 
-  if (!fontsLoaded) {
-    return null;
-  }
+export default function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home} />
-      </Tab.Navigator>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="LandingPage" component={LandingPage}/>
         <Stack.Screen name="Register" component={RegisterPage}/>
         <Stack.Screen name="Login" component={LoginPage}/>
+        <Stack.Screen name="HomeNavigator" component={HomeNavigator}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
