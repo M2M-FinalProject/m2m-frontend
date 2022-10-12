9c9257af5ce0511e532e39c7ad9dff27e045656f
mirzapm13 <mirzapm13@gmail.com>
2022-10-12 18:45:56 +0700

    fix navigation warning error

--- screens/MatchDetail.js
+++ screens/MatchDetail.js
@@ -4,9 +4,12 @@ import axios from 'axios'
 import { useFocusEffect } from "@react-navigation/native"
 import { useState, useEffect, useCallback } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { useNavigation } from '@react-navigation/native';
+
 
 
 export default function MatchDetail({ route, navigation }) {
+    // const navigation = useNavigation();
     const [detailData, setDetailData] = useState({})
     const [userId, setUserId] = useState('')
     const [accToken, setAccToken] = useState('')
